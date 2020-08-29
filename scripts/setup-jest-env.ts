23b33a5ef40ab0def3b1405e062a133b6cc18dbe
Bowen Liu <mr_lbw@outlook.com>
2020-08-29 23:43:48 +0800

    feat(eslint): build up (typescript-)estlint
    - https://github.com/typescript-eslint/typescript-eslint/tree/v4.0.0-rc/docs/getting-started/linting
    - https://github.com/typescript-eslint/typescript-eslint/blob/v4.0.0-rc/docs/getting-started/linting/MONOREPO.md
    - https://eslint.org/docs/user-guide/configuring#configuration-based-on-glob-patterns
    - https://github.com/prettier/eslint-plugin-prettier

--- scripts/setup-jest-env.ts
+++ scripts/setup-jest-env.ts
@@ -4,5 +4,6 @@ let warn: jest.SpyInstance
 
 beforeEach(() => {
   warn = jest.spyOn(console, 'warn')
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   warn.mockImplementation(() => {})
 })

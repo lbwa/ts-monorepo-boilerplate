ef5162b28c520ddaaa665a928c9cbfbfa07f4a05
Bowen Liu <mr_lbw@outlook.com>
2020-10-08 17:19:20 +0800

    feat: remove redundant files

--- scripts/setup-jest-env.ts
+++
@@ -1,9 +0,0 @@
-jest.setTimeout(5e3)
-
-let warn: jest.SpyInstance
-
-beforeEach(() => {
-  warn = jest.spyOn(console, 'warn')
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  warn.mockImplementation(() => {})
-})
